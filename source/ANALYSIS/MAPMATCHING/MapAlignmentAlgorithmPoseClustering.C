open-ms/open-ms-mirror

commit 4ca4b49c166e75dc4a9ce37d5ec883ec370550b9

Fixed: huge memory footprint of MapAlignmentPoseClustering has been solved for feature map alignment by reading the feature maps, condensing them to (more memory efficient) vectors of Peak2D and applying the resulting transformations to all maps in a second pass.
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9585 6adb6e08-d915-0410-941f-83917bcadc18

source/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithmPoseClustering.C
@@ -63,7 +63,8 @@ namespace OpenMS
 		reference_file_ = reference_file;
 	}
 
-	void MapAlignmentAlgorithmPoseClustering::alignPeakMaps(vector< MSExperiment<> >& maps, vector<TransformationDescription>& transformations)
+	void MapAlignmentAlgorithmPoseClustering::alignPeakMaps
+		(vector< MSExperiment<> >& maps, vector<TransformationDescription>& transformations)
 	{
 		// prepare transformations for output
 		transformations.clear();
@@ -104,9 +105,9 @@ namespace OpenMS
 
 
 	template <typename MapType>
-	void MapAlignmentAlgorithmPoseClustering::computeTransformations_(
-		vector<MapType>& maps, vector<TransformationDescription>& transformations,
-		Size reference_index, Size max_num_peaks_considered)
+	void MapAlignmentAlgorithmPoseClustering::computeTransformations_
+		(vector<MapType>& maps, vector<TransformationDescription>& transformations,
+		 Size reference_index, Size max_num_peaks_considered)
 	{
 		startProgress(0, 10 * maps.size(), "aligning input maps");
 
@@ -204,7 +205,8 @@ namespace OpenMS
 	}
 
 
-	void MapAlignmentAlgorithmPoseClustering::alignFeatureMaps(vector< FeatureMap<> >& maps, vector<TransformationDescription>& transformations)
+	void MapAlignmentAlgorithmPoseClustering::alignFeatureMaps
+		(vector< FeatureMap<> >& maps, vector<TransformationDescription>& transformations)
 	{
 		// prepare transformations for output
 		transformations.clear();
@@ -219,6 +221,48 @@ namespace OpenMS
 			}
 			maps.resize(maps.size() + 1);
 			FeatureXMLFile().load(reference_file_, maps.back());
+			reference_index = maps.size() - 1;
+		}
+    else if (reference_index_ == 0) // no reference given
+    {
+			// use map with highest number of features as reference:
+      Size max_count = 0;
+      for (Size m = 0; m < maps.size(); ++m)
+      {
+        if (maps[m].size() > max_count)
+        {
+          max_count = maps[m].size();
+          reference_index = m;
+        }
+      }
+    }
+
+		computeTransformations_(maps, transformations, reference_index);
+	}
+
+	void MapAlignmentAlgorithmPoseClustering::alignCompactFeatureMaps
+		(std::vector<std::vector<Peak2D> >& maps, std::vector<TransformationDescription>& transformations)
+	{
+		// prepare transformations for output
+		transformations.clear();
+
+		// reference map:
+		Size reference_index = reference_index_ - 1;
+		if (!reference_file_.empty())
+		{
+			if (FileHandler::getType(reference_file_) != FileTypes::FEATUREXML)
+			{
+				throw Exception::InvalidParameter(__FILE__, __LINE__, __PRETTY_FUNCTION__, "reference file must be of type featureXML in this case (same as input)");
+			}
+			maps.resize(maps.size() + 1);
+			FeatureMap<> feature_map;
+			FeatureXMLFile().load(reference_file_, feature_map);
+      FeatureMap<>::const_iterator it = feature_map.begin();
+			std::vector<Peak2D>::iterator c_it = maps.back().begin();
+      for (; it != feature_map.end(); ++it, ++c_it)
+      {
+        *c_it = reinterpret_cast<const Peak2D&>(*it);
+			}
 			reference_index = maps.size() - 1;
 		}
     else if (reference_index_ == 0) // no reference given

include/OpenMS/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithm.h
@@ -67,6 +67,13 @@ namespace OpenMS
 		virtual void alignPeakMaps(std::vector< MSExperiment<> >&, std::vector<TransformationDescription>&);
 
 		/**
+		@brief Aligns vectors of 2D peaks (memory efficient version of FeatureMap)
+
+		@exception Exception::NotImplemented is thrown if an algorithm cannot align feature maps
+		*/
+		virtual void alignCompactFeatureMaps(std::vector< std::vector<Peak2D> >&, std::vector<TransformationDescription>&);
+
+		/**
 		@brief Aligns feature maps
 
 		@exception Exception::NotImplemented is thrown if an algorithm cannot align feature maps

include/OpenMS/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithmPoseClustering.h
@@ -64,6 +64,9 @@ namespace OpenMS
 		virtual void alignPeakMaps(std::vector< MSExperiment<> >&, std::vector<TransformationDescription>&);
 
 		// Docu in base class
+		virtual void alignCompactFeatureMaps(std::vector<std::vector<Peak2D> >&, std::vector<TransformationDescription>&);
+
+		// Docu in base class
 		virtual void alignFeatureMaps(std::vector< FeatureMap<> >&, std::vector<TransformationDescription>&);
 
 		// Docu in base class

include/OpenMS/ANALYSIS/MAPMATCHING/PoseClusteringAffineSuperimposer.h
@@ -55,7 +55,8 @@ namespace OpenMS
 
    @ingroup MapAlignment
    */
-  class OPENMS_DLLAPI PoseClusteringAffineSuperimposer : public BaseSuperimposer
+  class OPENMS_DLLAPI PoseClusteringAffineSuperimposer 
+		: public BaseSuperimposer
   {
   public:
 

include/OpenMS/APPLICATIONS/MapAlignerBase.h
@@ -238,21 +238,30 @@ protected:
 		else if (in_type == FileTypes::FEATUREXML)
 		{
 			// load input
-			std::vector< FeatureMap<> > feat_maps(ins.size());
+			std::vector<std::vector<Peak2D> > feat_maps(ins.size());
 			FeatureXMLFile f;
 			// f.setLogType(log_type_); // TODO
 			progresslogger.startProgress(0, ins.size(), "loading input files");
 			for (Size i = 0; i < ins.size(); ++i)
 			{
 				progresslogger.setProgress(i);
-		    f.load(ins[i], feat_maps[i]);
+				FeatureMap<> feature_map;
+		    f.load(ins[i], feature_map);
+				feat_maps[i].resize(feature_map.size());
+
+				FeatureMap<>::const_iterator it = feature_map.begin();
+				std::vector<Peak2D>::iterator c_it = feat_maps[i].begin();
+				for (; it != feature_map.end(); ++it, ++c_it)
+				{
+          *c_it = reinterpret_cast<const Peak2D&>(*it);
+				}				
 			}
 			progresslogger.endProgress();
 
 			// try to align
 			try
 			{
-				alignment->alignFeatureMaps(feat_maps, transformations);
+				alignment->alignCompactFeatureMaps(feat_maps, transformations);
 			}
 			catch (Exception::NotImplemented&)
 			{
@@ -263,18 +272,23 @@ protected:
 			{
 				alignment->fitModel(model_type, model_params, transformations);
 			}
-			alignment->transformFeatureMaps(feat_maps, transformations);
+			// alignment->transformFeatureMaps(feat_maps, transformations);
 
 			// write output
 			progresslogger.startProgress(0, outs.size(), "writing output files");
 			for (Size i = 0; i < outs.size(); ++i)
 			{
 				progresslogger.setProgress(i);
+
+				FeatureMap<> feature_map;
+		    f.load(ins[i], feature_map);
+
+				alignment->transformSingleFeatureMap(feature_map, transformations[i]);
 				
 				//annotate output with data processing info
-				addDataProcessing_(feat_maps[i], getProcessingInfo_(DataProcessing::ALIGNMENT));
+				addDataProcessing_(feature_map, getProcessingInfo_(DataProcessing::ALIGNMENT));
 
-		    f.store(outs[i], feat_maps[i]);
+		    f.store(outs[i], feature_map);
 			}
 			progresslogger.endProgress();
 		}

include/OpenMS/KERNEL/ConsensusMap.h
@@ -427,6 +427,41 @@ namespace OpenMS
 				output_map.updateRanges();
 			}
 
+			/**
+				@brief Convert a vector of 2D Peaks (Peak2D) into a ConsensusMap.
+
+				Only the @p n most intense elements are copied.
+
+				Note: a new unique ID is generated for the consensus map.
+
+				@param input_map_index The index of the input map.
+				@param input_map The input map to be converted.
+				@param output_map The resulting ConsensusMap.
+				@param n The maximum number of elements to be copied.
+			*/
+			static void convert(UInt64 const input_map_index, std::vector<Peak2D>& input_map, ConsensusMap& output_map, Size n = -1)
+			{
+				// Clear the map and assign new ID.
+        output_map.setUniqueId();
+				output_map.clear(true);
+
+				// Determine the maximum size of the map and resize the output map accordingly.
+				if (n > input_map.size())
+				{
+					n = input_map.size();
+				}
+				output_map.reserve(n);
+
+				std::partial_sort(input_map.begin(), input_map.begin() + n, 
+													input_map.end(), reverseComparator(Peak2D::IntensityLess()));
+				for (Size element_index = 0; element_index < n; ++element_index)
+				{
+					output_map.push_back(ConsensusFeature(input_map_index, input_map[element_index], element_index));
+				}
+				output_map.getFileDescriptions()[input_map_index].size = n;
+				output_map.updateRanges();
+			}
+
       /**
 				@brief Convert a ConsensusMap to a FeatureMap (of any feature type).
 

source/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithm.C
@@ -66,6 +66,11 @@ namespace OpenMS
 		throw Exception::NotImplemented(__FILE__,__LINE__,__PRETTY_FUNCTION__);
 	}
 
+	void MapAlignmentAlgorithm::alignCompactFeatureMaps(vector<std::vector<Peak2D> >&, vector<TransformationDescription>&)
+	{
+		throw Exception::NotImplemented(__FILE__,__LINE__,__PRETTY_FUNCTION__);				
+	}
+
 	void MapAlignmentAlgorithm::alignFeatureMaps(vector< FeatureMap<> >&, vector<TransformationDescription>&)
 	{
 		throw Exception::NotImplemented(__FILE__,__LINE__,__PRETTY_FUNCTION__);				

source/ANALYSIS/MAPMATCHING/PoseClusteringAffineSuperimposer.C
@@ -139,7 +139,7 @@ namespace OpenMS
     // Optionally, we will write dumps of the hash table buckets.
     bool do_dump_buckets = false;
     String dump_buckets_basename;
-    if ( param_.getValue("dump_buckets") != "" )
+    if (param_.getValue("dump_buckets") != "")
     {
       do_dump_buckets = true;
       dump_buckets_basename = param_.getValue("dump_buckets");
@@ -149,7 +149,7 @@ namespace OpenMS
     // Even more optionally, we will write dumps of the hashed pairs.
     bool do_dump_pairs = false;
     String dump_pairs_basename;
-    if ( param_.getValue("dump_pairs") != "" )
+    if (param_.getValue("dump_pairs") != "")
     {
       do_dump_pairs = true;
       dump_pairs_basename = param_.getValue("dump_pairs");

commit 208fca162f0ffa495b871d7631a12bd894956c9a

** fixed mapalignment segfault introduced in r9585
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9590 6adb6e08-d915-0410-941f-83917bcadc18

source/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithmPoseClustering.C
@@ -254,14 +254,14 @@ namespace OpenMS
 			{
 				throw Exception::InvalidParameter(__FILE__, __LINE__, __PRETTY_FUNCTION__, "reference file must be of type featureXML in this case (same as input)");
 			}
-			maps.resize(maps.size() + 1);
 			FeatureMap<> feature_map;
 			FeatureXMLFile().load(reference_file_, feature_map);
+      maps.resize(maps.size() + 1);
+      maps.back().reserve(feature_map.size());
       FeatureMap<>::const_iterator it = feature_map.begin();
-			std::vector<Peak2D>::iterator c_it = maps.back().begin();
-      for (; it != feature_map.end(); ++it, ++c_it)
+      for (; it != feature_map.end(); ++it)
       {
-        *c_it = reinterpret_cast<const Peak2D&>(*it);
+        maps.back().push_back(reinterpret_cast<const Peak2D&>(*it));
 			}
 			reference_index = maps.size() - 1;
 		}